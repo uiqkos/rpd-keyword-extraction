6cfce7ef2f33f53251490d1f19c6f1b8260b919d
uiqkos <57287053+uiqkos@users.noreply.github.com>
2022-01-31 19:37:19 +0300

    tables v1

--- src/features/extract_structure.py
+++ src/features/extract_structure.py
@@ -1,8 +1,11 @@
+import logging
 import os
 
 import camelot
+import pdf2docx
 
 os.environ['LD_LIBRARY_PATH'] = 'C:\\Program Files\\gs\\gs9.55.0\\bin\\gsdll64.dll'
+logging.disable()
 
 from dataclasses import dataclass
 from os import PathLike
@@ -12,10 +15,12 @@ from typing import List, Iterator, Set
 import pandas as pd
 # import camelot
 import tabula
+from tqdm import tqdm as progressbar
 from docx import Document as read_docx
 from docx.document import Document
 
-from src.features.utils import convert_doc_to_docx, contains_marker, process, fix_column
+from src.features.utils import convert_doc_to_docx, contains_marker, process, fix_column, table_to_columns, \
+    convert_pdf_to_docx
 from src.settings import DATA_PATH, MARKERS
 from src.utils import list_files
 
@@ -30,23 +35,24 @@ class Extractor:
 
         if filepath.lower().endswith('.pdf'):
             # tables = camelot.read_pdf(filepath, pages='all')
-            tables = tabula.read_pdf(filepath, pages='all')
-
-            for table in tables:
-                # df: pd.DataFrame = table.df
-                df = table
-
-                for marker in self.markers:
-                    # rows
-                    # if idx := contains_marker(df.axes[0], marker):
-                    #     keywords.update(df.iloc[idx, :])
-
-                    for column_name in df:
-                        column = fix_column(df[column_name])
-                        if contains_marker(column, marker) or contains_marker([column], marker):
-                            keywords.update(column)
-
-            return keywords.difference(self.markers)
+            # tables = tabula.read_pdf(filepath, pages='all')
+            #
+            # for table in tables:
+            #     # df: pd.DataFrame = table.df
+            #     df = table
+            #
+            #     for marker in self.markers:
+            #         # rows
+            #         # if idx := contains_marker(df.axes[0], marker):
+            #         #     keywords.update(df.iloc[idx, :])
+            #
+            #         for column_name in df:
+            #             column = fix_column(df[column_name])
+            #             if contains_marker(column, marker) or contains_marker([column], marker):
+            #                 keywords.update(column)
+            #
+            # return keywords.difference(self.markers)
+            filepath = convert_pdf_to_docx(filepath)
 
         if filepath.lower().endswith('.doc'):
             filepath = convert_doc_to_docx(filepath)
@@ -57,11 +63,13 @@ class Extractor:
 
             for table in tables:
                 for marker in self.markers:
-                    if idx := contains_marker(table.rows, marker):
-                        keywords.update(table.row_cells(idx))
+                    # if idx := contains_marker(table.rows, marker):
+                    #     keywords.update(table.row_cells(idx))
 
-                    if idx := contains_marker(table.columns, marker):
-                        keywords.update(table.column_cells(idx))
+                    columns = table_to_columns(table)
+                    for column in columns:
+                        if contains_marker(column, marker):
+                            keywords.update(column)
 
             return keywords.difference(self.markers)
 
@@ -73,12 +81,40 @@ def files(docs_path=None) -> Iterator[PathLike]:
     return map(docs_path.joinpath, list_files(docs_path))
 
 
+def all_to_docx():
+    for file in progressbar(list(map(str, files())), ncols=100):
+        if file.lower().endswith('.pdf'):
+            convert_pdf_to_docx(file, True)
+        if file.lower().endswith('doc'):
+            convert_doc_to_docx(file, True)
+
+
 if __name__ == '__main__':
-    # extractor = Extractor(MARKERS)
+    # all_to_docx()
+    extractor = Extractor(MARKERS)
 
-    tables = camelot.read_pdf('D:\\projects\\rpd-keyword-extraction\\data\\documents\\(206832) 58 Разработка распределенных приложении.pdf')
+    keywords = {}
 
-    # print(extractor.extract())
+    for file in progressbar(list(files())[:1000], ncols=100):
+        try:
+            if not str(file).endswith('.docx'):
+                continue
+            keywords[file.name] = list(map(process, extractor.extract(file)))
+
+        except:
+            continue
+
+    p = DATA_PATH.joinpath('results')
+    p.mkdir(exist_ok=True)
+    with open(str(p.joinpath('tables.small.v1.csv')), 'w', encoding='utf-8') as f:
+        for k, v in keywords.items():
+            f.write(','.join([k, *v]) + '\n')
+
+    # pd.DataFrame(keywords).to_csv()
+
+    # tables = camelot.read_pdf('D:\\projects\\rpd-keyword-extraction\\data\\documents\\(206832) 58 Разработка распределенных приложении.pdf')
+
+    # print(list(map(process, extractor.extract('D:\\projects\\rpd-keyword-extraction\\data\\documents\\(206832) 58 Разработка распределенных приложении.docx'))))
 
     # print(fix_column(tables[1].iloc[:, -1]))
 
--- src/features/utils.py
+++ src/features/utils.py
@@ -1,20 +1,31 @@
+import os
 import re
-from operator import indexOf
+from operator import indexOf, attrgetter
 from typing import Union, Iterable, List
 
 import numpy as np
+import pdf2docx
+from docx.table import Table
 from win32com import client as wc
 
 
-def convert_doc_to_docx(filepath):
+def convert_doc_to_docx(filepath, remove=False):
     w = wc.Dispatch('Word.Application')
     doc = w.Documents.Open(filepath)
     doc.SaveAs(filepath + 'x', 16)
+    if remove:
+        os.remove(filepath)
     return filepath + 'x'
 
 
-def convert_pdf_to_docx(filepath):
-
+def convert_pdf_to_docx(filepath, remove=False):
+    cv = pdf2docx.Converter(filepath)
+    new_filepath = filepath.replace('.pdf', '.docx').replace('.PDF', '.docx')
+    cv.convert(new_filepath, start=0, end=None)
+    cv.close()
+    if remove:
+        os.remove(filepath)
+    return new_filepath
 
 
 def fix_column(column: Iterable[Union[str, type(np.nan)]]) -> List[str]:
@@ -46,10 +57,14 @@ def contains_marker(container, marker) -> Union[bool, type(None)]:
 
     def match(s): return (s := process(s)) in marker or marker in s
 
-    try:
-        idx = indexOf(list(map(match, container)), True)
+    # try:
+    #     idx = indexOf(list(map(match, container)), True)
+    #
+    # except ValueError:
+    #     idx = None
 
-    except ValueError:
-        idx = None
+    return any(map(match, container))
 
-    return idx
+
+def table_to_columns(table: Table) -> List[List[str]]:
+    return [list(map(attrgetter('text'), column.cells)) for column in table.columns]
